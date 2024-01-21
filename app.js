89fef9d50b1539f027ebc0174350ccb554ca2b23
Bikesh-ydv <149894118+Bikesh-ydv@users.noreply.github.com>
2024-01-21 22:46:50 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let input=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+btn.addEventListener("click",function()
+{
+    let item=document.createElement("li");
+    let delBtn=document.createElement("button");
+    delBtn.innerText= "delete";
+    delBtn.classList.add("delete");
+    item.innerText=input.value;
+    ul.appendChild(item);
+    item.appendChild(delBtn);
+    input.value="";
+});
+ul.addEventListener("click",function(event)
+{
+    if(event.target.nodeName=="BUTTON")
+    {
+       let par =event.target.parentElement;
+        par.remove();
+    }
+});
